93b8a0975c80ab94447b4bd7734d5a3a531a475b
supascooopa <mnalptekin1@gmail.com>
2022-06-29 14:37:39 +0300

    hid the secret key

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ from imei_processor import imei_machine
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config["SECRET_KEY"] = "SUPER_SECRET_KEY"
+app.config["SECRET_KEY"] = os.environ.get("secret_key")
 app.config["UPLOAD_FOLDER"] = os.path.abspath("static\\files")  # configure upload folder
 app.config["MAX_CONTENT_LENGTH"] = 400 * 1024  # Maximum upload file size is 400kb
 db = SQLAlchemy(app)
