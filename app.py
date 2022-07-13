f8671c57fbdb153d840e1db254f682da0167574e
supascoopa <mnalptekin1@gmail.com>
2022-07-12 21:51:21 -0700

    integrating database uri

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ from imei_processor import imei_machine
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SECRET_KEY"] = os.environ.get("secret_key")
 app.config["UPLOAD_FOLDER"] = os.path.abspath("static\\files")  # configure upload folder
